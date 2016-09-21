1006c959aea0e2e908838f7f5ecc4e7a233b9fe5
alay <liu_anguo@qq.com>
2016-09-21 17:23:24 +0800

    测试git

---
+++ src/Class/Test.java
@@ -0,0 +1,13 @@
+package Class;
+
+/**
+ * @author Alay
+ * @created 2016-09-21 17:18
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("33");
+    }
+
+}
